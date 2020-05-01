a1c9264da4d5b26f90d4d3fd1c291a821d3bafd4
Romain Racamier-Lafon <romain.racamier@gmail.com>
2020-05-01 15:30:40 +0200

    remove unused tailwing config props

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,11 @@ module.exports = {
   purge: {
     mode: 'all',
     enabled: true,
-    content: [
-      './src/*.js',
-      './public/*.html'
-    ]
+    content: ['./src/*.js', './public/*.html']
   },
   theme: {
-    extend: {},
     container: {
       center: true
     }
   },
-  variants: {},
-  plugins: []
 }
