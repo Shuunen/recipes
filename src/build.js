440f90c7e18c1d2e894d082f2d1d87214494ecae
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-04-02 19:32:05 +0200

    enhance build & watch

--- src/build.js
+++ src/build.js
@@ -1,10 +1,43 @@
-const { writeFileSync, readFileSync } = require('fs')
-const { Converter } = require('showdown')
-const showdownToc = require('showdown-toc')
-const converter = new Converter({ ghCompatibleHeaderId: true, headerLevelStart: 2, extensions: [showdownToc()] })
-const md = readFileSync('public/recipes.md')
-const today = (d => `${(d.getMonth() + 1 + '').padStart(2, '0')}/${(d.getDate() + '').padStart(2, '0')}/${d.getFullYear()}`)(new Date())
-const content = converter.makeHtml('# Sommaire\n[toc]\n' + md)
-const template = readFileSync('src/page.html') + ''
-const html = template.replace('{content}', content).replace('{nbLines}', 10).replace('{updated}', today)
-writeFileSync('public/index.html', html)
+const {mdToHtml} = require('./md-to-html')
+const chokidar = require('chokidar')
+const fs = require('fs')
+const globcat = require('globcat')
+
+let watch = false
+let focus = '**'
+for (const argument of process.argv.slice(2)) {
+  if (argument === '--watch') {
+    watch = true
+  } else {
+    focus = argument
+  }
+}
+
+const target = `src/${focus}/*.md`
+const buildHtml = (updateTemplate = false) => globcat(target, (error, md) => {
+  if (error) {
+    throw error
+  }
+
+  fs.writeFileSync('public/index.html', mdToHtml(md, updateTemplate))
+})
+const copyStyles = () => fs.copyFile('src/styles.css', 'public/styles.css', error => {
+  if (error) {
+    throw error
+  }
+})
+
+if (watch) {
+  console.log(`watching : ${target}`)
+  chokidar.watch(['src/*.html', target]).on('change', path => {
+    console.log(`${path} changed, rebuilding...`)
+    buildHtml(path.includes('.html'))
+  })
+  chokidar.watch('src/*.css').on('change', () => {
+    console.log('styles changed, copying...')
+    copyStyles()
+  })
+}
+
+buildHtml(true)
+copyStyles()
