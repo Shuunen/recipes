c559cb775d919d21df9dcfba8c175d7f439bea21
Romain Racamier-Lafon <romain.racamier@gmail.com>
2021-06-19 16:10:53 +0200

    use newer stack & switch to module

--- src/build-md.js
+++ src/build-md.js
@@ -1,11 +1,12 @@
-const { readFileSync } = require('fs')
-const { Converter } = require('showdown')
-const showdownToc = require('showdown-toc')
-const converter = new Converter({ghCompatibleHeaderId: true, headerLevelStart: 2, extensions: [showdownToc()]})
+import { readFileSync } from 'fs'
+import Showdown from 'showdown'
+import ShowdownToc from 'showdown-toc'
+
+const converter = new Showdown.Converter({ ghCompatibleHeaderId: true, headerLevelStart: 2, extensions: [ShowdownToc()] })
 const today = (d => `${(String(d.getMonth() + 1)).padStart(2, '0')}/${(String(d.getDate())).padStart(2, '0')}/${d.getFullYear()} à ${(String(d.getHours())).padStart(2, '0')}h${(String(d.getMinutes())).padStart(2, '0')}`)(new Date())
 let template = ''
 
-module.exports.mdToHtml = (md, updateTemplate = false) => {
+export function mdToHtml (md, updateTemplate = false) {
   if (updateTemplate) template = String(readFileSync('src/layout.html'))
   const content = converter.makeHtml('# Sommaire\n[toc]\n' + md)
   const html = template.replace('{content}', content).replace('{updated}', today)
